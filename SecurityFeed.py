cd0752b38e1afa7651efc873aa24bab688036b0b
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2017-11-02 22:21:33 +0800

    Security Feed Detection Added

---
+++ SecurityFeed.py
@@ -0,0 +1,46 @@
+import cv2
+
+video = cv2.VideoCapture("Video/CCTV.mp4")
+#video = cv2.VideoCapture(0)
+
+firstFrame = None
+
+while(video.isOpened()):
+    ok, frame = video.read()
+    text = "Unoccupied"
+    
+    if not ok:
+        break
+    
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+    
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+    
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    _, cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for c in cnts:
+        if cv2.contourArea(c) < 500:
+            continue
+        
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        text = "Occupied"
+        
+    cv2.putText(frame, "Status: {}".format(text), (10, 20),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    
+    cv2.imshow("Security Feed", frame)
+    #cv2.imshow("Thresh", thresh)
+    #cv2.imshow("Frame Delta", frameDelta)
+    
+    k = cv2.waitKey(1) & 0xff
+    if k == 27 : break
+cv2.waitKey()
+cv2.destroyAllWindows()
