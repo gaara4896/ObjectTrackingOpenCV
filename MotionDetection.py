354b2c4bd9cd4ef8205b574701bc1d3d2035e6d9
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2017-11-02 23:39:16 +0800

    MotionDetection Implemented

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,3 +1,37 @@
 import sys
 sys.path.insert(0, 'utils')
 import FaceDetection
+import DifferentTracking
+import MotionTracking
+import cv2
+
+#video = cv2.VideoCapture("Video/CarGame.avi")
+video = cv2.VideoCapture(0)
+
+if not video.isOpened():
+    print("Could not open video")
+    sys.exit()
+    
+ok, frame = video.read()
+
+while cv2.waitKey() != 27:
+    MotionTracking.addTracker(frame, cv2.selectROI(frame, False))
+
+while True:
+    ok, frame = video.read()
+    if not ok:
+        break
+    
+    motion, frame = MotionTracking.update(frame)
+    face, frame = FaceDetection.update(frame)
+    
+    if not motion and not face:    
+        frame = DifferentTracking.update(frame)
+    
+    cv2.imshow("Tracking", frame)
+ 
+    k = cv2.waitKey(1) & 0xff
+    if k == 27 : break
+
+cv2.waitKey()
+cv2.destroyAllWindows()
--- utils/FaceDetection.py
+++ utils/FaceDetection.py
@@ -5,6 +5,8 @@ face_cascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.
 def update(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    successFlag = False
     for x, y, w, h in faces:
         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-    return frame
+        successFlag = True
+    return successFlag, frame
