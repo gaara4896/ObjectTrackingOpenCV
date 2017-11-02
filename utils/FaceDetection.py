2df7d01993c4cae180a7f1c3dc72d57603d6c6b2
Ng Wei Shyang <15033996@imail.sunway.edu.my>
2017-11-02 23:14:32 +0800

    Update FaceDetection update module

--- utils/FaceDetection.py
+++ utils/FaceDetection.py
@@ -5,4 +5,6 @@ face_cascade = cv2.CascadeClassifier("resources/haarcascade_frontalface_default.
 def update(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    return faces
+    for x, y, w, h in faces:
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    return frame
